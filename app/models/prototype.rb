306bfbb6f749bc2d679f43ddbdbf1f6c94ad66b2
中平 慶春 <nakadaira@nakahiranoMacBook-Pro-2.local>
2021-07-30 14:31:52 +0900

    dependent: :destroyの追加

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -4,8 +4,8 @@ class Prototype < ApplicationRecord
   validates :concept, presence: true
   validates :image, presence: true
 
-  has_many :comments
+  has_many :comments, dependent: :destroy
   belongs_to :user
-  
+
   has_one_attached :image
 end
