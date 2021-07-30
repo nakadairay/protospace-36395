25bf4a07ae768b5aa524c91ddcdc3cc4e1cc6bfa
中平 慶春 <nakadaira@nakahiranoMacBook-Pro-2.local>
2021-07-30 17:55:45 +0900

    修正依頼3 再修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -32,7 +32,7 @@ class PrototypesController < ApplicationController
   def update
     @prototype = Prototype.find(params[:id])
     if @prototype.update(prototype_params)
-      redirect_to prototype_path(prototype.id)
+      redirect_to prototype_path(@prototype.id)
     else
       render action: :edit
     end
